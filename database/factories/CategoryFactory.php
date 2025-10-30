6e9dc8cd4084e5177bb5959fcba4fe5ea9d8f477
Maximo Gutierrez <maximogtzog@gmail.com>
2025-10-29 20:03:02 -0600

    Comment on CategoryFactory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
 // this is eloquent
+// this is eloquent
+// this is eloquent
 class CategoryFactory extends Factory
 {
     protected $model = Category::class;
