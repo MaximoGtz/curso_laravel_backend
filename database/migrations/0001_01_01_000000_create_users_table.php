84ced6a9f9d20723a0192f0d82824a1385330652
Maximo Gutierrez <maximogtzog@gmail.com>
2025-11-27 19:37:27 -0600

    nnada

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 // laravel
+// laravel
+// laravel
+// laravel
 return new class extends Migration
 {
     /**
