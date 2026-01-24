ff48350d7185b23f7e91457783e95dd1a3c56dc8
Maximo Gutierrez <maximogtzog@gmail.com>
2026-01-23 19:54:44 -0600

    Deleted a laravel default comment in ClientFactory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -3,9 +3,6 @@
 namespace Database\Factories;
 use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
- */
 class ClientFactory extends Factory
 {
     protected $model = Client::class;
